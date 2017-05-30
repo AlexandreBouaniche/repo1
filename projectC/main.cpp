5fd4012d36009b95bd2c0367f50ff7d35c161fc6
Alexandre Bouaniche <bouaniche@MacBook-Pro-de-coria-2.local>
2017-05-30 18:11:39 +0200

    test fonction ajouteDeux ok

--- projectC/main.cpp
+++ projectC/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
+#include "maths.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    int n(0);
+    cout << "Entrez le nombre initial: " << endl;
+    cin >> n;
+    n = ajouteDeux(n);
+    cout << "resultat: " << n << endl;
+
     return 0;
 }
